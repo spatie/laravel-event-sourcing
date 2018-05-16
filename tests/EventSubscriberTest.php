68454876be2aef1bffea4b02ae4b233cbd5373c9
Freek <freek@spatie.be>
2018-05-16 16:49:36 +0200

    add test

--- tests/EventSubscriberTest.php
+++ tests/EventSubscriberTest.php
@@ -3,6 +3,7 @@
 namespace Spatie\EventSaucer\Tests;
 
 use Spatie\EventSaucer\StoredEvent;
+use Spatie\EventSaucer\Tests\Events\DoNotStoreThisEvent;
 use Spatie\EventSaucer\Tests\Models\Account;
 use Spatie\EventSaucer\Tests\Events\MoneyAdded;
 
@@ -19,10 +20,18 @@ class EventSubscriberTest extends TestCase
     }
 
     /** @test */
-    public function it_will_log_events()
+    public function it_will_log_events_that_are_marked_with_should_be_stored()
     {
         event(new MoneyAdded($this->account, 1234));
 
-        $this->assertCount(1, StoredEvent::all());
+        $this->assertCount(1, StoredEvent::get());
+    }
+
+    /** @test */
+    public function it_will_not_store_events_without_the_ShouldBeStored_interface()
+    {
+        event(new DoNotStoreThisEvent());
+
+        $this->assertCount(0, StoredEvent::get());
     }
 }
---
+++ tests/Events/DoNotStoreThisEvent.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Spatie\EventSaucer\Tests\Events;
+
+class DoNotStoreThisEvent
+{
+
+}
