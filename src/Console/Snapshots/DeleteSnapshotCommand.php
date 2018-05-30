7636081001e3a36d85b542531fee030af81d1290
freek <freek@spatie.be>
2018-05-30 10:59:15 +0200

    delete snapshot scaffold

--- src/Console/Snapshots/DeleteSnapshotCommand.php
+++ src/Console/Snapshots/DeleteSnapshotCommand.php
@@ -10,14 +10,44 @@ class DeleteSnapshotCommand extends Command
 
     protected $description = 'Delete snapshots';
 
+    /** @var \Illuminate\Support\Collection */
+    protected  $snapshots;
+
+    public function __construct()
+    {
+        $this->snapshots = $this->snapshotRepository->get();
+    }
+
     public function handle()
+    {
+        if ($this->snapshots->isEmpty()) {
+            $this->warn("There currently are no snapshots. You can take a snapshot by running `php artisan event-projector:create-snapshot`.");
+
+            return;
+        }
+
+        $this->displaySnapshots();
+
+        $snapshotNumber = $this->ask('Which snapshot would you like to delete?');
+
+        if (! $snapshot = $this->snapshots->get($snapshotNumber)) {
+            $this->error("There is no snapshot for that number.");
+
+            return;
+        }
+
+        $snapshot->delete();
+
+        $this->comment("Snapshot number {$snapshotNumber} deleted!");
+    }
+
+    public function displaySnapshots()
     {
         $titles = ['Number', 'Projector', 'Last processed event id', 'Created at', 'Name'];
 
-        $rows = $this->snapshotRepository->get()->map(function (Snapshot $snapshot) {
-            static $number = 0;
+        $rows = $this->snapshots->map(function (Snapshot $snapshot, int $index) {
             return [
-                $number++,
+                $index,
                 $snapshot->projectorName(),
                 $snapshot->lastProcessedEventId(),
                 $snapshot->createdAt(),
@@ -26,7 +56,5 @@ class DeleteSnapshotCommand extends Command
         });
 
         $this->table($titles, $rows);
-
-        $snapshotNumber = $this->ask('Which snapshot would you like to delete?');
     }
 }
