3ba57fba411a8a2ea093e236570bee026b7914b6
freek <freek@spatie.be>
2019-05-18 13:53:15 +0200

    wip

--- event-handlers.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-  'Spatie\\EventProjector\\Tests\\TestClasses\\Projectors\\BalanceProjector' => 'Spatie\\EventProjector\\Tests\\TestClasses\\Projectors\\BalanceProjector',
-  'Spatie\\EventProjector\\Tests\\TestClasses\\Reactors\\BrokeReactor' => 'Spatie\\EventProjector\\Tests\\TestClasses\\Reactors\\BrokeReactor',
-];
