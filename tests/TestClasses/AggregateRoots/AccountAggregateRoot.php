4f9cd9fcfce677da65481b97a52533174b3c5993
Adrian Brown <adrianrb93@gmail.com>
2019-09-04 23:52:08 +1000

    Add test.

--- tests/TestClasses/AggregateRoots/AccountAggregateRoot.php
+++ tests/TestClasses/AggregateRoots/AccountAggregateRoot.php
@@ -4,6 +4,7 @@ namespace Spatie\EventProjector\Tests\TestClasses\AggregateRoots;
 
 use Spatie\EventProjector\AggregateRoot;
 use Spatie\EventProjector\Tests\TestClasses\AggregateRoots\StorableEvents\MoneyAdded;
+use Spatie\EventProjector\Tests\TestClasses\AggregateRoots\StorableEvents\MoneyMultiplied;
 
 final class AccountAggregateRoot extends AggregateRoot
 {
@@ -16,8 +17,20 @@ final class AccountAggregateRoot extends AggregateRoot
         return $this;
     }
 
+    public function multiplyMoney(int $amount): self
+    {
+        $this->recordThat(new MoneyMultiplied($amount));
+
+        return $this;
+    }
+
     public function applyMoneyAdded(MoneyAdded $event)
     {
         $this->balance += $event->amount;
+    }
+
+    public function applyMoneyMultiplied(MoneyMultiplied $event)
+    {
+        $this->balance *= $event->amount;
     }
 }
