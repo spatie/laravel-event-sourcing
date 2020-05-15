11f631105956608b0952d35b9304816fdb51f224
Craig Morris <craig.michael.morris@gmail.com>
2020-05-15 19:30:50 +0100

    StyleCI

--- src/StoredEvent.php
+++ src/StoredEvent.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Arr;
 use Spatie\EventSourcing\EventSerializers\EventSerializer;
 use Spatie\EventSourcing\Exceptions\InvalidStoredEvent;
 use Spatie\EventSourcing\Facades\Projectionist;
-use Spatie\SchemalessAttributes\SchemalessAttributes;
 
 class StoredEvent implements Arrayable
 {
