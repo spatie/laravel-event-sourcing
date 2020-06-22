2efad47d413c2c2178397fd71b7c631535bdfb62
Craig Morris <craig.michael.morris@gmail.com>
2020-06-22 11:15:21 +0000

    Simplify test class

--- tests/TestClasses/EventSerializer/UpgradeSerializer.php
+++ tests/TestClasses/EventSerializer/UpgradeSerializer.php
@@ -11,42 +11,17 @@ use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer as SymfonySerializer;
 
-class UpgradeSerializer implements EventSerializer
+class UpgradeSerializer extends JsonEventSerializer
 {
-    private SymfonySerializer $serializer;
-
-    public function __construct()
-    {
-        $encoders = [new JsonEncoder()];
-        $normalizers = [new DateTimeNormalizer(), new ObjectNormalizer()];
-
-        $this->serializer = new SymfonySerializer($normalizers, $encoders);
-    }
-
-    public function serialize(ShouldBeStored $event): string
-    {
-        /*
-         * We call __sleep so `Illuminate\Queue\SerializesModels` will
-         * prepare all models in the event for serialization.
-         */
-        if (method_exists($event, '__sleep')) {
-            $event->__sleep();
-        }
-
-        $json = $this->serializer->serialize($event, 'json');
-
-        return $json;
-    }
-
     public function deserialize(string $eventClass, string $json, string $metadata = null): ShouldBeStored
     {
-        $restoredEvent = $this->serializer->deserialize($json, $eventClass, 'json');
+        $event = parent::deserialize($eventClass, $json, $metadata);
 
         $metadata = json_decode($metadata, true);
         if ($metadata['version'] < 2) {
-            $restoredEvent->value = $restoredEvent->value->setTimezone(new DateTimeZone('UTC'));
+            $event->value = $event->value->setTimezone(new DateTimeZone('UTC'));
         }
 
-        return unserialize(serialize($restoredEvent));
+        return $event;
     }
 }
