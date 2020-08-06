58414648e59479494b0dda07fa80b5cb506c2734
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2020-08-06 18:04:57 +0200

    Allow protected applyX methods

--- src/AggregateRoots/AggregateRoot.php
+++ src/AggregateRoots/AggregateRoot.php
@@ -195,12 +195,19 @@ abstract class AggregateRoot
 
         $applyingMethodName = "apply{$camelCasedBaseName}";
 
-        if (method_exists($this, $applyingMethodName)) {
+        $reflectionClass = new ReflectionClass($this);
+
+        $applyMethodExists = $reflectionClass->hasMethod($applyingMethodName);
+        $applyMethodIsPublic = $applyMethodExists && $reflectionClass->getMethod($applyingMethodName)->isPublic();
+
+        if ($applyMethodExists && $applyMethodIsPublic) {
             try {
                 app()->call([$this, $applyingMethodName], ['event' => $event]);
             } catch (BindingResolutionException $exception) {
                 $this->$applyingMethodName($event);
             }
+        } else if ($applyMethodExists) {
+            $this->$applyingMethodName($event);
         }
 
         $this->appliedEvents[] = $event;
