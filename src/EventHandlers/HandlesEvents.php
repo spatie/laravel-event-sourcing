eecae72cdde05f7a1a979ab8c63d7a8d373f5059
Brent Roose <brent@spatie.be>
2020-11-04 15:36:39 +0100

    Add ListensTo attribute

--- src/EventHandlers/HandlesEvents.php
+++ src/EventHandlers/HandlesEvents.php
@@ -4,9 +4,11 @@ namespace Spatie\EventSourcing\EventHandlers;
 
 use Exception;
 use Illuminate\Support\Collection;
+use ReflectionAttribute;
 use ReflectionClass;
 use ReflectionMethod;
 use ReflectionParameter;
+use Spatie\EventSourcing\Attributes\ListensTo;
 use Spatie\EventSourcing\Exceptions\InvalidEventHandler;
 use Spatie\EventSourcing\StoredEvents\ShouldBeStored;
 use Spatie\EventSourcing\StoredEvents\StoredEvent;
@@ -59,7 +61,7 @@ trait HandlesEvents
         $handlesEvents = collect($this->handlesEvents ?? [])
             ->mapWithKeys(function (string $handlerMethod, $eventClass) {
                 if (is_numeric($eventClass)) {
-                    return [$handlerMethod => 'on'.ucfirst(class_basename($handlerMethod))];
+                    return [$handlerMethod => 'on' . ucfirst(class_basename($handlerMethod))];
                 }
 
                 return [$eventClass => $handlerMethod];
@@ -74,23 +76,40 @@ trait HandlesEvents
 
     private function autoDetectHandlesEvents(): Collection
     {
-        return collect((new ReflectionClass($this))->getMethods())
+        return collect((new ReflectionClass($this))->getMethods(ReflectionMethod::IS_PUBLIC))
             ->flatMap(function (ReflectionMethod $method) {
                 $method = new ReflectionMethod($this, $method->name);
-                if (! $method->isPublic()) {
-                    return;
-                }
 
-                $eventClass = collect($method->getParameters())
-                    ->map(fn (ReflectionParameter $parameter) => optional($parameter->getType())->getName())
-                    ->first(fn ($typeHint) => is_subclass_of($typeHint, ShouldBeStored::class));
+                $listensToAttributes = $method->getAttributes(ListensTo::class);
 
-                if (! $eventClass) {
-                    return null;
-                }
-
-                return [$eventClass => $method->name];
+                return empty($listensToAttributes)
+                    ? $this->determineAcceptedEventsFromTypeHint($method)
+                    : $this->determineAcceptedEventsFromAttribute($method);
             })
+            ->mapWithKeys(fn(array $items) => $items)
             ->filter();
+    }
+
+    private function determineAcceptedEventsFromTypeHint(ReflectionMethod $method): ?array
+    {
+        $eventClass = collect($method->getParameters())
+            ->map(fn(ReflectionParameter $parameter) => optional($parameter->getType())->getName())
+            ->first(fn($typeHint) => is_subclass_of($typeHint, ShouldBeStored::class));
+
+        if (! $eventClass) {
+            return null;
+        }
+
+        return [
+            [$eventClass => $method->name],
+        ];
+    }
+
+    private function determineAcceptedEventsFromAttribute(ReflectionMethod $method): array
+    {
+        return collect($method->getAttributes(ListensTo::class))
+            ->map(fn(ReflectionAttribute $attribute) => $attribute->newInstance())
+            ->map(fn(ListensTo $listensTo) => [$listensTo->eventClass => $method->name])
+            ->toArray();
     }
 }
