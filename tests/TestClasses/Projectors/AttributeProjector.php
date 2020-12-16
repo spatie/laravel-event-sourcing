0cf01acb97d04e9ef3ba4e07ba76e417805ec71c
Brent Roose <brent@spatie.be>
2020-12-16 14:16:54 +0100

    Improvements to the Handles attribute

--- tests/TestClasses/Projectors/AttributeProjector.php
+++ tests/TestClasses/Projectors/AttributeProjector.php
@@ -2,9 +2,8 @@
 
 namespace Spatie\EventSourcing\Tests\TestClasses\Projectors;
 
-use Spatie\EventSourcing\Attributes\ListensTo;
+use Spatie\EventSourcing\Attributes\Handles;
 use Spatie\EventSourcing\EventHandlers\Projectors\Projector;
-use Spatie\EventSourcing\Tests\TestClasses\Events\EmptyAccountEvent;
 use Spatie\EventSourcing\Tests\TestClasses\Events\MoneyAddedEvent;
 use Spatie\EventSourcing\Tests\TestClasses\Events\MoneyAddedEventWithQueueOverride;
 use Spatie\EventSourcing\Tests\TestClasses\Events\MoneySubtractedEvent;
@@ -13,17 +12,14 @@ class AttributeProjector extends Projector
 {
     public static array $handledEvents = [];
 
-    #[ListensTo(MoneyAddedEvent::class)]
-    public function handleSingleAttributeEvent($event)
+    #[Handles(MoneyAddedEvent::class)]
+    public function handleSingleAttributeEvent($event): void
     {
         self::$handledEvents[$event::class] = $event;
     }
 
-    #[
-        ListensTo(MoneySubtractedEvent::class),
-        ListensTo(MoneyAddedEventWithQueueOverride::class),
-    ]
-    public function handleMultiAttributeEvent($event)
+    #[Handles(MoneySubtractedEvent::class, MoneyAddedEventWithQueueOverride::class)]
+    public function handleMultiAttributeEvent($event): void
     {
         self::$handledEvents[$event::class] = $event;
     }
