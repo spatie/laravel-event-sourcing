2a41add75dac76500a89d57620f7bf2742f9277f
Brent Roose <brent@spatie.be>
2020-12-17 15:13:37 +0100

    EventQuery test

--- tests/TestClasses/Projectors/ReportEventQuery.php
+++ tests/TestClasses/Projectors/ReportEventQuery.php
@@ -3,24 +3,30 @@
 namespace Spatie\EventSourcing\Tests\TestClasses\Projectors;
 
 use Spatie\EventSourcing\Attributes\Handles;
+use Spatie\EventSourcing\EventHandlers\AppliesEvents;
 use Spatie\EventSourcing\EventHandlers\Projectors\EventQuery;
-use Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder;
+use Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEvent;
 use Spatie\EventSourcing\Tests\TestClasses\AggregateRoots\StorableEvents\MoneyAdded;
 
 class ReportEventQuery extends EventQuery
 {
-    public int $money = 0;
+    use AppliesEvents;
+
+    private int $money = 0;
 
     public function __construct(private string $minDate)
     {
-        parent::__construct();
     }
 
-    protected function query(EloquentStoredEventQueryBuilder $query): EloquentStoredEventQueryBuilder
+    public function __invoke(): int
     {
-        return $query
+        EloquentStoredEvent::query()
             ->whereEvent(MoneyAdded::class)
-            ->whereDate('created_at', '>=', $this->minDate);
+            ->whereDate('created_at', '>=', $this->minDate)
+            ->cursor()
+            ->each(fn (EloquentStoredEvent $eloquentStoredEvent) => $this->apply($eloquentStoredEvent->toStoredEvent()));
+
+        return $this->money;
     }
 
     #[Handles(MoneyAdded::class)]
