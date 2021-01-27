cc4cbc7700ff6e4398f31c8dce021d0e1cfede39
Brent Roose <brent@spatie.be>
2021-01-27 12:47:18 +0100

    Cleanup and test fixes

--- tests/TestClasses/Projectors/ProjectorThatThrowsAnException.php
+++ tests/TestClasses/Projectors/ProjectorThatThrowsAnException.php
@@ -8,8 +8,15 @@ use Spatie\EventSourcing\Tests\TestClasses\Events\MoneyAddedEvent;
 
 class ProjectorThatThrowsAnException extends BalanceProjector implements ShouldQueue
 {
+    public static int $exceptionsHandled = 0;
+
     public function onMoneyAdded(MoneyAddedEvent $event)
     {
         throw new Exception('Computer says no.');
+    }
+
+    public function handleException(Exception $exception): void
+    {
+        self::$exceptionsHandled += 1;
     }
 }
