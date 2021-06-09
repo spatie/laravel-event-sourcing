064744d15dac611a5f6c2309a4e108fafa1c0efe
freek <freek@spatie.be>
2021-06-09 11:01:50 +0200

    nitpick

--- src/StoredEvents/Exceptions/InvalidStoredEvent.php
+++ src/StoredEvents/Exceptions/InvalidStoredEvent.php
@@ -9,6 +9,9 @@ class InvalidStoredEvent extends Exception
 {
     public static function couldNotUnserializeEvent(StoredEvent $storedEvent, Exception $serializerException): self
     {
-        return new static("Failed to unserialize an event with class `{$storedEvent->event_class}` on stored event with id `{$storedEvent->id}`. Are you sure that event class exists? ", 0, $serializerException);
+        return new static(
+            "Failed to unserialize an event with class `{$storedEvent->event_class}` on stored event with id `{$storedEvent->id}`. Are you sure that event class exists? ",
+            previous:  $serializerException
+        );
     }
 }
