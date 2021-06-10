b7f422f7f32616809576ee4f7fc0f61be79046b6
Santosh Khanal <santosh.khanal55@gmail.com>
2021-06-10 17:56:58 +0545

    fix test for migration include

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -51,11 +51,11 @@ abstract class TestCase extends Orchestra
         });
 
         Schema::dropIfExists('stored_events');
-        include_once __DIR__.'/../stubs/create_stored_events_table.php.stub';
+        include_once __DIR__.'/../database/migrations/create_stored_events_table.php.stub';
         (new CreateStoredEventsTable())->up();
 
         Schema::dropIfExists('snapshots');
-        include_once __DIR__.'/../stubs/create_snapshots_table.php.stub';
+        include_once __DIR__.'/../database/migrations/create_snapshots_table.php.stub';
         (new CreateSnapshotsTable())->up();
 
         Schema::dropIfExists('other_stored_events');
