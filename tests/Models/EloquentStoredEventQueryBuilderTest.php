37370a93b5d10a87be28fbd9ec3ca91157ee9a3b
brendt <brendt@users.noreply.github.com>
2022-03-04 05:18:22 +0000

    Fix styling

--- tests/Models/EloquentStoredEventQueryBuilderTest.php
+++ tests/Models/EloquentStoredEventQueryBuilderTest.php
@@ -11,11 +11,13 @@ class EloquentStoredEventQueryBuilderTest extends TestCase
     public function it_constrains_to_property_value()
     {
         $expected = EloquentStoredEvent::query()->whereJsonContains(
-            column: 'event_properties->otherEntityId', value: 10
+            column: 'event_properties->otherEntityId',
+            value: 10
         );
 
         $actual = EloquentStoredEvent::query()->wherePropertyIs(
-            property: 'otherEntityId', value: 10
+            property: 'otherEntityId',
+            value: 10
         );
 
         $this->assertEquals($expected, $actual);
@@ -25,11 +27,13 @@ class EloquentStoredEventQueryBuilderTest extends TestCase
     public function it_constrains_to_property_value_difference()
     {
         $expected = EloquentStoredEvent::query()->whereJsonDoesntContain(
-            column: 'event_properties->name', value: 'Johnson'
+            column: 'event_properties->name',
+            value: 'Johnson'
         );
 
         $actual = EloquentStoredEvent::query()->wherePropertyIsNot(
-            property: 'name', value: 'Johnson'
+            property: 'name',
+            value: 'Johnson'
         );
 
         $this->assertEquals($expected, $actual);
