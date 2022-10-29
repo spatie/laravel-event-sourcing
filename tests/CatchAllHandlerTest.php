1772d36cbeda85f112a33826c943b187d1fa599c
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-10-29 17:08:46 +0300

    refactor CatchAllHandlerTest

--- tests/CatchAllHandlerTest.php
+++ tests/CatchAllHandlerTest.php
@@ -5,18 +5,14 @@ namespace Spatie\EventSourcing\Tests;
 use Spatie\EventSourcing\Facades\Projectionist;
 use Spatie\EventSourcing\Tests\TestClasses\Events\TestEvent;
 use Spatie\EventSourcing\Tests\TestClasses\Reactors\CatchAllReactor;
+use function PHPUnit\Framework\assertEquals;
 
-class CatchAllHandlerTest extends TestCase
-{
-    /** @test */
-    public function all_events_are_handled()
-    {
-        CatchAllReactor::$log = [];
+test('all events are handled', function () {
+    CatchAllReactor::$log = [];
 
-        Projectionist::addReactor(CatchAllReactor::class);
+    Projectionist::addReactor(CatchAllReactor::class);
 
-        event(new TestEvent());
+    event(new TestEvent());
 
-        $this->assertEquals([TestEvent::class], CatchAllReactor::$log);
-    }
-}
+    assertEquals([TestEvent::class], CatchAllReactor::$log);
+});
