89f191f2ca5abad46236e982d22adb6173d2f290
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-10-29 21:54:22 +0300

    refactor VersionedEventTest

--- tests/VersionedEventTest.php
+++ tests/VersionedEventTest.php
@@ -7,71 +7,67 @@ use Spatie\EventSourcing\Attributes\EventVersion;
 use Spatie\EventSourcing\EventSerializers\JsonEventSerializer;
 use Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEvent;
 use Spatie\EventSourcing\StoredEvents\ShouldBeStored;
+use function PHPUnit\Framework\assertEquals;
 
-class VersionedEventTest extends TestCase
-{
-    /** @test */
-    public function the_event_version_is_saved_in_the_database()
+beforeAll(function () {
+    #[
+        EventVersion(2),
+        EventSerializer(VersionedEventSerializer::class),
+    ]
+    class VersionedEvent extends ShouldBeStored
     {
-        $event = new VersionedEvent('uuid-1');
-
-        event($event);
-
-        /** @var \Spatie\EventSourcing\StoredEvents\StoredEvent $storedEvent */
-        $storedEvent = EloquentStoredEvent::find($event->storedEventId())->toStoredEvent();
-
-        $this->assertEquals(2, $storedEvent->event_version);
-    }
-
-    /** @test */
-    public function a_versioned_event_can_be_restored()
-    {
-        /** @var \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEvent $storedEventV1 */
-        $storedEventV1 = EloquentStoredEvent::create([
-            "id" => 1,
-            "aggregate_uuid" => null,
-            "aggregate_version" => null,
-            "event_version" => 1,
-            "event_class" => "Spatie\\EventSourcing\\Tests\\VersionedEvent",
-            "event_properties" => ['name' => 'event-1'],
-            "meta_data" => [],
-            "created_at" => now(),
-        ]);
-
-        /** @var \Spatie\EventSourcing\Tests\VersionedEvent $event */
-        $event = $storedEventV1->toStoredEvent()->event;
-
-        $this->assertEquals('event-1', $event->uuid);
-    }
-}
-
-#[
-    EventVersion(2),
-    EventSerializer(VersionedEventSerializer::class),
-]
-class VersionedEvent extends ShouldBeStored
-{
-    public function __construct(
-        public string $uuid
-    ) {
-    }
-}
-
-class VersionedEventSerializer extends JsonEventSerializer
-{
-    public function deserialize(
-        string $eventClass,
-        string $json,
-        int $version,
-        string $metadata = null
-    ): VersionedEvent {
-        $data = json_decode($json, true);
-
-        if ($version === 1) {
-            $data['uuid'] = $data['name'] ?? '';
-            unset($data['name']);
+        public function __construct(
+            public string $uuid
+        ) {
         }
-
-        return new VersionedEvent(...$data);
     }
-}
+
+    class VersionedEventSerializer extends JsonEventSerializer
+    {
+        public function deserialize(
+            string $eventClass,
+            string $json,
+            int $version,
+            string $metadata = null
+        ): VersionedEvent {
+            $data = json_decode($json, true);
+
+            if ($version === 1) {
+                $data['uuid'] = $data['name'] ?? '';
+                unset($data['name']);
+            }
+
+            return new VersionedEvent(...$data);
+        }
+    }
+});
+
+test('the event version is saved in the database', function () {
+    $event = new VersionedEvent('uuid-1');
+
+    event($event);
+
+    /** @var \Spatie\EventSourcing\StoredEvents\StoredEvent $storedEvent */
+    $storedEvent = EloquentStoredEvent::find($event->storedEventId())->toStoredEvent();
+
+    assertEquals(2, $storedEvent->event_version);
+});
+
+test('a versioned event can be restored', function () {
+    /** @var \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEvent $storedEventV1 */
+    $storedEventV1 = EloquentStoredEvent::create([
+        "id" => 1,
+        "aggregate_uuid" => null,
+        "aggregate_version" => null,
+        "event_version" => 1,
+        "event_class" => "Spatie\\EventSourcing\\Tests\\VersionedEvent",
+        "event_properties" => ['name' => 'event-1'],
+        "meta_data" => [],
+        "created_at" => now(),
+    ]);
+
+    /** @var \Spatie\EventSourcing\Tests\VersionedEvent $event */
+    $event = $storedEventV1->toStoredEvent()->event;
+
+    assertEquals('event-1', $event->uuid);
+});
