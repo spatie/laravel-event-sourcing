c67086a11b65667167d6f63478d5835ae0163b0d
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-10-29 09:09:33 +0300

    refactor CommandHandlerTest

--- tests/AggregateRoots/CommandHandlerTest.php
+++ tests/AggregateRoots/CommandHandlerTest.php
@@ -9,101 +9,101 @@ use Spatie\EventSourcing\Commands\CommandBus;
 use Spatie\EventSourcing\Commands\HandledBy;
 use Spatie\EventSourcing\StoredEvents\ShouldBeStored;
 use Spatie\EventSourcing\Tests\TestCase;
+use function PHPUnit\Framework\assertCount;
 
-class CommandHandlerTest extends TestCase
-{
-    protected const UUID = 'cart-uuid';
-
-    /** @test */
-    public function command_is_dispatched_to_aggregate()
+beforeAll(function () {
+    #[HandledBy(CartForCommand::class)]
+    class AddItem
     {
-        $bus = new CommandBus();
-
-        CartForCommand::retrieve(self::UUID)->persist();
-
-        // Assert that commands are dispatched to entities
-        $bus->dispatch(new AddItem(
-            self::UUID,
-            'name'
-        ));
-
-        $this->assertCount(1, CartForCommand::retrieve(self::UUID)->cartItems->items);
-
-        // Assert that commands are dispatched to the AR iteself
-        $bus->dispatch(new ClearCart(
-            self::UUID,
-        ));
-
-        $this->assertCount(0, CartForCommand::retrieve(self::UUID)->cartItems->items);
+        public function __construct(
+            #[AggregateUuid] public string $cartUuid,
+            public string $name
+        ) {
+        }
     }
-}
 
-#[HandledBy(CartForCommand::class)]
-class AddItem
-{
-    public function __construct(
-        #[AggregateUuid] public string $cartUuid,
-        public string $name
-    ) {
-    }
-}
-
-#[HandledBy(CartForCommand::class)]
-class ClearCart
-{
-    public function __construct(
-        #[AggregateUuid] public string $cartUuid
-    ) {
-    }
-}
-
-class CartForCommand extends AggregateRoot
-{
-    // Public for testing
-    public CartItemsForCommand $cartItems;
-
-    public function __construct()
+    #[HandledBy(CartForCommand::class)]
+    class ClearCart
     {
-        $this->cartItems = new CartItemsForCommand($this);
+        public function __construct(
+            #[AggregateUuid] public string $cartUuid
+        ) {
+        }
     }
 
-    public function clear(ClearCart $clearCart): self
+    class CartForCommand extends AggregateRoot
     {
-        $this->recordThat(new CartClearedForCommand());
+        // Public for testing
+        public CartItemsForCommand $cartItems;
 
-        return $this;
+        public function __construct()
+        {
+            $this->cartItems = new CartItemsForCommand($this);
+        }
+
+        public function clear(ClearCart $clearCart): self
+        {
+            $this->recordThat(new CartClearedForCommand());
+
+            return $this;
+        }
+
+        public function applyClear(CartClearedForCommand $event): void
+        {
+            $this->cartItems = new CartItemsForCommand($this);
+        }
     }
 
-    public function applyClear(CartClearedForCommand $event): void
+    class CartItemsForCommand extends AggregatePartial
     {
-        $this->cartItems = new CartItemsForCommand($this);
+        // Public for testing
+        public array $items = [];
+
+        public function addItem(AddItem $addItem)
+        {
+            $this->recordThat(new ItemAddedForCommand($addItem->name));
+        }
+
+        public function applyAddItem(ItemAddedForCommand $itemAdded)
+        {
+            $this->items[] = $itemAdded->name;
+        }
     }
-}
 
-class CartItemsForCommand extends AggregatePartial
-{
-    // Public for testing
-    public array $items = [];
-
-    public function addItem(AddItem $addItem)
+    class ItemAddedForCommand extends ShouldBeStored
     {
-        $this->recordThat(new ItemAddedForCommand($addItem->name));
+        public function __construct(
+            public string $name
+        ) {
+        }
     }
 
-    public function applyAddItem(ItemAddedForCommand $itemAdded)
+    class CartClearedForCommand extends ShouldBeStored
     {
-        $this->items[] = $itemAdded->name;
     }
-}
+});
 
-class ItemAddedForCommand extends ShouldBeStored
-{
-    public function __construct(
-        public string $name
-    ) {
-    }
-}
+beforeEach(function () {
+    $this->UUID = 'cart-uuid';
+});
 
-class CartClearedForCommand extends ShouldBeStored
-{
-}
+test('command is dispatched to aggregate', function () {
+    $bus = new CommandBus();
+
+    CartForCommand::retrieve($this->UUID)->persist();
+
+    // Assert that commands are dispatched to entities
+    $bus->dispatch(new AddItem(
+        $this->UUID,
+        'name'
+    ));
+
+    assertCount(1, CartForCommand::retrieve($this->UUID)->cartItems->items);
+
+    // Assert that commands are dispatched to the AR iteself
+    $bus->dispatch(new ClearCart(
+        $this->UUID,
+    ));
+
+    assertCount(0, CartForCommand::retrieve($this->UUID)->cartItems->items);
+});
