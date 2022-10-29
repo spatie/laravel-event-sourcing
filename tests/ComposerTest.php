a0a055087c28def57bcac48a2ec7bf0fc140a19e
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-10-29 17:09:34 +0300

    refactor ComposerTest

--- tests/ComposerTest.php
+++ tests/ComposerTest.php
@@ -5,21 +5,16 @@ namespace Spatie\EventSourcing\Tests;
 use Illuminate\Support\Str;
 use Spatie\EventSourcing\Support\Composer;
 
-class ComposerTest extends TestCase
-{
-    /** @test */
-    public function it_can_get_all_loaded_files()
-    {
-        $pathToComposerJson = __DIR__.'/../composer.json';
+test('it can get all loaded files', function () {
+    $pathToComposerJson = __DIR__.'/../composer.json';
 
-        $files = Composer::getAutoloadedFiles($pathToComposerJson);
+    $files = Composer::getAutoloadedFiles($pathToComposerJson);
 
-        $files = array_map(function (string $path) {
-            return Str::after($path, $this->pathToTests());
-        }, $files);
+    $files = array_map(function (string $path) {
+        return Str::after($path, $this->pathToTests());
+    }, $files);
 
-        $this->assertEquals([
-            '/TestClasses/AutoDiscoverEventHandlers/functions.php',
-        ], $files);
-    }
-}
+    $this->assertEquals([
+        '/TestClasses/AutoDiscoverEventHandlers/functions.php',
+    ], $files);
+});
