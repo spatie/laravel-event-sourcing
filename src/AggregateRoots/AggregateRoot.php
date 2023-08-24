378b786a095d51eba4a4ac984906bcf2506eff02
sebastiandedeyne <sebastiandedeyne@gmail.com>
2023-08-24 11:50:35 +0200

    CS

--- src/AggregateRoots/AggregateRoot.php
+++ src/AggregateRoots/AggregateRoot.php
@@ -258,7 +258,9 @@ abstract class AggregateRoot
             ->reject(fn (Method $method) => in_array($method->getName(), ['handleCommand', 'recordThat', 'apply', 'tap']))
             ->accepts($event)
             ->all()
-            ->each(fn (Method $method) => $this->{$method->getName()}($event));
+            ->each(function (Method $method) use ($event) {
+                return $this->{$method->getName()}($event);
+            });
 
         $this->appliedEvents[] = $event;
 
