7aa01d7d33552d954f7a652b415ea02783ffba3b
sebastiandedeyne <sebastiandedeyne@gmail.com>
2023-08-24 11:50:18 +0200

    Use better-types accepts instead of acceptsTypes in AggregateRoot::apply to make behavior consistent with HandlesEvents

--- src/AggregateRoots/AggregateRoot.php
+++ src/AggregateRoots/AggregateRoot.php
@@ -256,7 +256,7 @@ abstract class AggregateRoot
             ->public()
             ->protected()
             ->reject(fn (Method $method) => in_array($method->getName(), ['handleCommand', 'recordThat', 'apply', 'tap']))
-            ->acceptsTypes([$event::class])
+            ->accepts($event)
             ->all()
             ->each(fn (Method $method) => $this->{$method->getName()}($event));
 
