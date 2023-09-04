071b1330ea25419f8e239059304b108b1b9008bf
Chris Lloyd <chrislloyd403@gmail.com>
2023-09-04 10:30:26 +0100

    Added also to HandlesEvents

--- src/EventHandlers/HandlesEvents.php
+++ src/EventHandlers/HandlesEvents.php
@@ -16,6 +16,7 @@ trait HandlesEvents
         return Handlers::new($this)
             ->public()
             ->protected()
+            ->reject(fn (Method $method) => $method->accepts(null))
             ->accepts($storedEvent->event)
             ->all()
             ->isNotEmpty();
@@ -28,6 +29,7 @@ trait HandlesEvents
         Handlers::new($this)
             ->public()
             ->protected()
+            ->reject(fn (Method $method) => $method->accepts(null))
             ->accepts($event)
             ->all()
             ->each(function (Method $method) use ($event) {
